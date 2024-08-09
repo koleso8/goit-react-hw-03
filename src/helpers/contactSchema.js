dc3b95d26b978bd77a3d30f68174e7d7ad0705d3
koleso8 <dev.koleso@gmail.com>
2024-08-09 19:46:21 +0300

    changed shema

--- src/helpers/contactSchema.js
+++ src/helpers/contactSchema.js
@@ -3,12 +3,12 @@ import * as Yup from 'yup';
 export const contactSchema = Yup.object().shape({
   name: Yup.string()
     .trim()
-    .min(3, 'Must be at least 8 characters')
-    .max(50, 'Must be less  than 20 characters')
+    .min(3, 'Must be at least 3 characters')
+    .max(20, 'Must be less  than 20 characters')
     .required('Name is required'),
   number: Yup.string()
     .trim()
-    .min(3, 'Must be at least 8 characters')
-    .max(50, 'Must be less  than 20 characters')
+    .min(3, 'Must be at least 3 characters')
+    .max(20, 'Must be less  than 20 characters')
     .required('Number is required'),
 });
